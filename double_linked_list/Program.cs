b37a86a8ca38c9458cea3f5848b4d1ceaeab3832
Adam Anderson <adam.anderson.ft21@mail.umy.ac.id>
2022-11-16 16:23:49 +0700

    membuat class Node

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace double_linked_list
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+        /*Node class represents the node of doubly linked list.
+         * It Consist of the information part and links to
+         * its succeeding and preceeding nodes
+         * in terms of next and previous nodes. */
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
     }
 }
